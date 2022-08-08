8f1133e4e49906ba29e7954bbabc3a4c7a38c075
Ayushmaan <ayushmaanjangid2002@gmail.com>
2022-08-08 21:59:29 +0530

    First-4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from 'react';
+import UseCard from './hooks/useCard';
 
 function App() {
+  // let card = useCard;
   return (
     <React.Fragment>
-      
+      {/* {card} */}
+      <UseCard/>
     </React.Fragment>
   );
 }
---
+++ src/hooks/useCard.js
@@ -0,0 +1,10 @@
+// import { render } from '@testing-library/react';
+import React from 'react'
+
+const UseCard = () => {
+    return(<div>
+        smaple
+    </div>)
+}
+
+export default UseCard;
